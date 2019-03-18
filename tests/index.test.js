95d7a81bed0b2f36b14b72cfea9baf95e29e6ea8
Albino Tonnina <albinotonnina@gmail.com>
2019-03-18 21:30:25 +0000

    update deps and tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -10,84 +10,76 @@ const readFile = location =>
   )
 
 describe('Asset', () => {
-  it(
-    'should wrap the js asset with comments',
-    async () => {
-      // Init bundler
-      const bundler = new Bundler(path.join(__dirname, './file.js'), {
+  it('should wrap the js asset with comments', async () => {
+    // Init bundler
+    const bundler = new Bundler(path.join(__dirname, './file.js'), {
+      outDir: path.join(__dirname, 'dist'),
+      production: true,
+      watch: false,
+      cache: false,
+      hmr: false,
+      logLevel: 0
+    })
+    // Register plugin
+    wrapperPlugin(bundler)
+
+    // Bundle everything
+    const bundle = await bundler.bundle()
+    assertBundle(bundle, {
+      type: 'js',
+      name: 'file.js',
+      childBundler: [
+        {
+          type: 'map'
+        }
+      ]
+    })
+
+    const expected = [
+      expect.stringContaining('/* parcel-plugin-wrapper - 0.2.2 */'),
+      expect.stringContaining('// The End')
+    ]
+
+    const fileContent = (await readFile(bundle.name)).toString()
+    //   Check asset content
+    expect({
+      bundleHead: fileContent,
+      bundleFoot: fileContent
+    }).toEqual({
+      bundleHead: expect.stringContaining(
+        '/* parcel-plugin-wrapper - 0.2.2 */'
+      ),
+      bundleFoot: expect.stringContaining('The End')
+    })
+  }, 25000)
+
+  it('should work with multiple entry points', async () => {
+    // Init bundler
+    const bundler = new Bundler(
+      [path.join(__dirname, './file.js'), path.join(__dirname, './file2.js')],
+      {
         outDir: path.join(__dirname, 'dist'),
         production: true,
         watch: false,
         cache: false,
         hmr: false,
         logLevel: 0
-      })
-      // Register plugin
-      wrapperPlugin(bundler)
+      }
+    )
+    // Register plugin
+    wrapperPlugin(bundler)
 
-      // Bundle everything
-      const bundle = await bundler.bundle()
-      assertBundle(bundle, {
-        type: 'js',
-        name: 'file.js',
-        childBundler: [
-          {
-            type: 'map'
-          }
-        ]
-      })
+    // Bundle everything
+    await bundler.bundle()
 
-      const expected = [
-        expect.stringContaining('/* parcel-plugin-wrapper - 0.2.0 */'),
-        expect.stringContaining('// The End')
-      ]
+    //   Check asset content
+    const filename = path.join(__dirname, './dist/file.js')
+    const fileContent = (await readFile(filename)).toString()
+    const fileLines = fileContent.split('\n')
+    const firstLine = fileLines[0]
+    const lastLine = fileLines[fileLines.length - 1]
 
-      const fileContent = (await readFile(bundle.name)).toString()
-      //   Check asset content
-      expect({
-        bundleHead: fileContent,
-        bundleFoot: fileContent
-      }).toEqual({
-        bundleHead: expect.stringContaining(
-          '/* parcel-plugin-wrapper - 0.2.0 */'
-        ),
-        bundleFoot: expect.stringContaining('The End')
-      })
-    },
-    25000
-  )
-
-  it(
-    'should work with multiple entry points',
-    async () => {
-      // Init bundler
-      const bundler = new Bundler(
-        [path.join(__dirname, './file.js'), path.join(__dirname, './file2.js')],
-        {
-          outDir: path.join(__dirname, 'dist'),
-          production: true,
-          watch: false,
-          cache: false,
-          hmr: false,
-          logLevel: 0
-        }
-      )
-      // Register plugin
-      wrapperPlugin(bundler)
-
-      // Bundle everything
-      await bundler.bundle()
-
-      //   Check asset content
-      const filename = path.join(__dirname, './dist/file.js')
-      const fileContent = (await readFile(filename)).toString()
-      const fileLines = fileContent.split('\n')
-      const firstLine = fileLines[0]
-      const lastLine = fileLines[fileLines.length - 1]
-
-      expect(firstLine).toMatch('/* parcel-plugin-wrapper - 0.2.0 */')
-      expect(lastLine).toMatch('The End')
-    },
-    25000
-  )
+    expect(firstLine).toMatch('/* parcel-plugin-wrapper - 0.2.2 */')
+    expect(lastLine).toMatch('The End')
+  }, 25000)
 })
